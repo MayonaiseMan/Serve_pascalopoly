5c3a2206687f1a3e33838acafbfc4f504b4757bc
Tommaso Brandinelli <48562227+MayonaiseMan@users.noreply.github.com>
2021-05-10 17:10:15 +0200

    server

---
+++ Serve_pascalopoly/Connessione.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Serve_pascalopoly
+{
+    class Connessione
+    {
+        MainWindow _main;
+        const int LOCAL_PORT = 50000;
+
+        public Connessione(MainWindow main)
+        {
+
+
+            try
+            {
+                //get local machine ip
+                LocalIp = IPAddress.Parse(GetLocalIPAddress());
+
+                //generate Listener foc tcp connection
+                Listener = new TcpListener(LocalIp, LOCAL_PORT);
+
+                _main = main;
+
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            } 
+        }
+
+        public Dictionary<string,TcpClient> ClientConnessi
+        {
+            get;
+            private set;
+        }
+
+        public async void Start()
+        {
+            await Task.Run(() => {
+                Listener.Start();
+                while(true)
+                {
+                    _main.WriteConsole("In attesa di connessione...");
+
+
+                    //waiting for connection
+                    TcpClient client = Listener.AcceptTcpClient();
+                    string tmp = "client" + (ClientConnessi.Count + 1);
+                    ClientConnessi.Add(tmp,client);
+                    
+                    _main.WriteConsole("nuova connessione con: " + client.Client.LocalEndPoint.ToString());
+               }
+            });
+        }
+
+        public async void ListenToTheStereo(TcpClient client) //yeah i know, i have several mental problem
+        {
+            await Task.Run(() => {
+                byte[] buffer;    
+                
+            
+            });
+        }
+
+        public IPAddress LocalIp
+        {
+            get;
+            private set;
+        }
+
+        public TcpListener Listener
+        {
+            get;
+            private set;
+        }
+
+        
+        
+
+
+        public byte[] GetBufferHead()
+        {
+            if(Buffer.Count > 0)
+            {
+                byte[] vs = Buffer.Dequeue();
+                return vs;
+            }
+            else
+            {
+                return new byte[0];
+            }
+            
+
+        }
+
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+
+    
+}
---
+++ Serve_pascalopoly/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Serve_pascalopoly
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
+        public void Disambiguatore(byte[] vs)
+        {
+            //TODO: roba
+        }
+
+        public void CheckConsole()
+        {
+            string s = "";
+            if (out_lbl.Content != null)
+            {
+                s = (string)out_lbl.Content;
+                string[] vs = s.Split('\n');
+                if (vs.Length >= 10)
+                {
+                    out_lbl.Content = "";
+                }
+            }
+        }
+
+        public void WriteConsole(string s)
+        {
+            CheckConsole();
+            out_lbl.Content += s + "\n";
+        }
+    }
+}
